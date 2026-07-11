4e28fc87c6a57df3f56c7722011a5ac734c0b95f
Gustavo-87 <Guadpilo87@hotmail.com>
2026-07-10 20:25:15 -0500

    Agrega middleware de rol para crear tareas

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('tareas', TaskController::class);
+    Route::middleware('rol:admin')->group(function () {
+        Route::get('/tareas/create', [TaskController::class, 'create'])->name('tareas.create');
+        Route::post('/tareas', [TaskController::class, 'store'])->name('tareas.store');
+    });
+
+    Route::resource('tareas', TaskController::class)->except(['create', 'store']);
 });
 
 require __DIR__.'/auth.php';
